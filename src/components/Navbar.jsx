02b546229d0c7876c1285edece278495a51d14ee
prabhat-3 <72614694+Aditya-2bharti@users.noreply.github.com>
2023-03-20 14:13:56 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [active, setActive] = useState(" ")
   const [toggle, setToggle] = useState(false)
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to='/'
@@ -19,7 +19,7 @@ const Navbar = () => {
             window.scroll(0, 0);
           }}>
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Aditya &nbsp;<span className=''>Bharti</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Aditya &nbsp; <span className=''>Bharti</span></p>
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -57,3 +57,15 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
+
+
+ 
+ 
